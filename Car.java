07381410a684038fcd9e81ae5bcbe43e913eb986
TanyaCST <schen@smith.edu>
2024-02-19 17:58:28 -0500

    Car.java done with unfinished main. Just started on passenger.java.

--- Car.java
+++ Car.java
@@ -1,3 +1,74 @@
+import java.util.ArrayList;
+
+
 public class Car {
+    int maxCapacity;
+
+    // An `ArrayList` where it will store the `Passenger`s currently onboard
+    ArrayList<Passenger> currentOnboard = new ArrayList<Passenger>();
+
+    public Car(int capacityMax){
+        currentOnboard = new ArrayList<Passenger>(capacityMax);
+        
+    }
+
+    public int getCapacity(){
+        return maxCapacity;
+
+    }
+
+    public int seatsRemaining(){
+        int seatsRemaing = maxCapacity - currentOnboard.size();
+        return seatsRemaing;
+        
+    }
+
+    public boolean addPassenger(Passenger p){
+        int seatsRemaing = seatsRemaining();
+        if(seatsRemaing > 0){
+            currentOnboard.add(p);
+            if(currentOnboard.contains(p)){
+                return true;
+            }
+    
+            else{
+                return false;
+            }
+        }
+        else{
+            return false;
+        }
+    }
+
+    public boolean removePassenger(Passenger p){
+        currentOnboard.remove(p);
+
+        if(currentOnboard.contains(p)){
+            return false;
+        }
+
+        else{
+            return true;
+        }
+    }
+
+    private void printManifest(){
+        if(seatsRemaining() == maxCapacity){
+            System.out.println("This car is EMPTY.");
+        }
+        else{
+            System.out.println(currentOnboard);
+        }
+    }
+    
+    public static void main(String[] args) {
+        Car myCar = new Car(50);
+        System.out.println(myCar.getCapacity());
+        myCar.addPassenger(null);
+        myCar.removePassenger(null);
+        myCar.printManifest();
+
+    }
+    
 
 }
--- Passenger.java
+++ Passenger.java
@@ -5,4 +5,8 @@ public class Passenger {
     public Passenger(String name) {
         this.name = name;
     }
+
+    public void boardCar(Car c){
+        c.addPassenger(this);
+    }
 }
